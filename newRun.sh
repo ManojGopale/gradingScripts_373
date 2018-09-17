8186fb150e6b7f3d02057ed5268bca9dd0716c29
ual-laptop <ual-laptop@dhcp-10-134-250-58.uawifi.arizona.edu>
2018-09-17 14:49:51 -0700

    Newrun and Lib for JUnit5 jupiter added

--- newRun.sh
+++ newRun.sh
@@ -19,23 +19,40 @@ else
   mkdir bin
 fi
 
+## Linking libraries rather than copying them
+linkFiles=`ls -ld ~/Manoj/gradingScripts_373/newLib_Jupiter/* | awk '{print $9}'`
+## You need to be in lib directory before you execute this loop
+## Junit5 is superset of JUnit4 libraries. So no need to have separate loop for JUnit5
+## Just link all the libraries
+#for i in $linkFiles;
+#do 
+#	cmd="ln -s $i";
+#	eval $cmd; 
+#done
+
+
 if [ -e lib ]; then
   echo "Directory lib exists in ${PWD}"
-  if [ -e lib/junit-4.10.jar ]; then
+	## If lib is present and any of the libraries are not linked then re-link everything
+  if [ -e lib/junit-4.13-20180730.170926-150.jar -a -e lib/hamcrest-core-1.3.jar -a -e lib/apiguardian-1.0.0-20170908.181424-6.jar \
+		-a -e lib/junit-jupiter-api-5.1.0-20180218.190948-382.jar -a -e lib/junit-jupiter-engine-5.1.0-20180218.190950-382.jar \
+		-a -e lib/junit-jupiter-migrationsupport-5.1.0-20180218.190951-382.jar -a -e lib/junit-jupiter-params-5.1.0-20180218.190953-382.jar \
+		-a -e lib/junit-platform-commons-1.1.0-20180218.190954-382.jar -a -e lib/junit-platform-engine-1.1.0-20180218.190958-381.jar \
+		-a -e lib/junit-platform-launcher-1.1.0-20180218.191059-380.jar -a -e lib/junit-platform-runner-1.1.0-20180218.191100-380.jar \
+		-a -e lib/junit-platform-suite-api-1.1.0-20180218.191102-380.jar -a -e lib/junit-vintage-engine-5.1.0-20180218.191104-236.jar \
+		-a -e lib/opentest4j-1.0.0-20170730.202628-48.jar -a -e lib/apiguardian-api-1.0.0-20170909.133433-2.jar ]; then
     echo "Junit is linked in lib directory"
   else
     cd lib
     echo "Linking junit in lib"
-    ln -s ~/Downloads/junit-4.10.jar
-    cd ../
-  fi
-
-  if [ -e lib/hamcrest-core-1.3.jar ]; then
-    echo "Hamcrest is linked in lib directory"
-  else
-    cd lib
-    echo "Linking hamcrest in lib"
-    ln -s ~/Downloads/hamcrest-core-1.3.jar
+		## Links all the libraries in the newLib_Jupiter directory
+		## Junit5 is superset of JUnit4 libraries. So no need to have separate loop for JUnit5
+		## Just link all the libraries
+		for i in $linkFiles;
+		do 
+			cmd="ln -s $i";
+			eval $cmd; 
+		done
     cd ../
   fi
 else
@@ -43,18 +60,26 @@ else
   mkdir lib
   cd lib
   ## Create softlinks in the lib directory
-  ln -s ~/Downloads/junit-4.10.jar
-  ln -s ~/Downloads/hamcrest-core-1.3.jar
+	## Links all the libraries in the newLib_Jupiter directory
+	## Junit5 is superset of JUnit4 libraries. So no need to have separate loop for JUnit5
+	## Just link all the libraries
+	for i in $linkFiles;
+	do 
+		cmd="ln -s $i";
+		eval $cmd; 
+	done
   cd ../
 fi
 
 ## Create soft links in the lib directory
 
 ## Compiling Person and PersonTest
-javac -d bin -cp src $srcFile
 echo "javac -d bin -cp src $srcFile"
+javac -d bin -cp src $srcFile
 
-javac -d bin -cp bin:lib/junit-4.10.jar:lib/hamcrest-core-1.3.jar $testFile
+echo "javac -d bin -cp bin:lib/* $testFile"
+javac -d bin -cp bin:lib/* $testFile
 
 ## Running the JUnit test
-java -cp bin:lib/junit-4.10.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore $testClass
+echo "java -cp bin:lib/* org.junit.runner.JUnitCore $testClass"
+java -cp bin:lib/* org.junit.runner.JUnitCore $testClass
